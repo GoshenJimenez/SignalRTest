339ff9d32e8b79776b00e6cda80bcbab67e17c8d
Goshen Jimenez <gjimenez@abcidee.com>
2022-08-21 17:08:12 +0800

    Signal R

---
+++ GoshenJimenez.SignalRTest/SignalRTest/Pages/Chatter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignalRTest.Pages
+{
+    public class ChatterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
